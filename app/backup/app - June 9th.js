e9f667676f2a128fe004717db5e84abdba3977ed
Murali Mankhala <Murali Mankhala@AXATech-IN.INTRAXA>
2017-06-13 20:47:21 +0530

    UI Router and Assignment 4

---
+++ app/backup/app - June 9th.js
@@ -0,0 +1,26 @@
+'use strict';
+angular.module('confusionApp', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $routeProvider
+            // route for the contactus page
+            .when('/contactus', {
+                templateUrl : 'contactus.html',
+                controller  : 'ContactController'
+            })
+            // route for the menu page
+            .when('/menu', {
+                templateUrl : 'menu.html',
+                controller  : 'MenuController'
+            })
+            // route for the dish details page
+            .when('/menu/:id', {
+                templateUrl : 'dishdetail.html',
+                controller  : 'DishDetailController'
+            })
+            .otherwise('/contactus');
+    })
+
+
+;
